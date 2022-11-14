11461d6a2555d5cd82c606dc7a6ca22852c3ee8c
I-Minevski <114011447+I-Minevski@users.noreply.github.com>
2022-11-14 08:35:57 +0200

    Update horner.py
    Asking the user for input of the polynome coefficients.

--- horner.py
+++ horner.py
@@ -5,3 +5,11 @@ def horner_method(coef, n):
         result = n*result + int(coef[i])
         new_poly_coef.append(result)
     return new_poly_coef
+
+
+equation_coefficients = []
+while True:
+    coefficient = input("Enter a valid coefficient. If done type [end]. ")
+    if coefficient == "end":
+        break
+    equation_coefficients.append(int(coefficient))
